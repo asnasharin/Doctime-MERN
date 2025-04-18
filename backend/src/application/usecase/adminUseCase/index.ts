52ee98d47b4ad62cdd9cd9346f26970fbce7cfeb
asna sharin <asnasherin1233@gmail.com>
2025-04-18 23:19:34 +0530

    feat: add admin usecase for checking kyc status

--- backend/src/application/usecase/adminUseCase/index.ts
+++ backend/src/application/usecase/adminUseCase/index.ts
@@ -1,11 +1,24 @@
-// authUseCase.js
+// // authUseCase.js
 
 import adminLoginUseCase from "./adminLoginUsecase";
 import adminLogoutUseCase from "./adminLogoutUsecase";
-
+import adminUserUseCase from "./adminUserUseCase";
+import adminDoctorUseCase from "./adminDoctorUseCase";
+import getKycDetailsUseCase from "./getkycDetailsUseCase";
+import kycStatusUseCase from "./kycStatusUseCase";
+import handleUserBlockUseCase from "./handleUserBlockUseCase";
+import handleDoctorBlockUseCase from "./handleDoctorBlockUseCase";
+import getAllAppoinmentsUseCase from "./getAllAppoinmentsUseCase";
 export {
     adminLoginUseCase,
+    adminUserUseCase,
+    adminDoctorUseCase,
+    getKycDetailsUseCase,
+    kycStatusUseCase,
+    handleUserBlockUseCase,
+    handleDoctorBlockUseCase,
     adminLogoutUseCase,
+    getAllAppoinmentsUseCase
 
 
 
