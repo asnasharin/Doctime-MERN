9dd283cc2d353a3430295c45f69b2430b4ca87ef
asna sharin <asnasherin1233@gmail.com>
2025-04-24 22:15:26 +0530

    feat: add doctor card component

---
+++ frontend/src/components/Cards/Cards.tsx
@@ -0,0 +1,32 @@
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Cards.css';
+
+function Cards() {
+  return (
+    <div className="cards-container">
+      <div className="cards">
+        <div className="card red">
+          <Link to="/findDoctor">
+            <p className="tip">Find Your Doctor</p>
+          </Link>
+        </div>
+        <div className="card blue">
+          <Link to="">
+            <p className="tip">Book Appointment</p>
+          </Link>
+        </div>
+        <div className="card green">
+          <Link to="/myprofile">
+            <p className="tip">Your Profile</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
+
